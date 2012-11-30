029f8a99ecf6579903a989ebc7313b6233c04991
Gabriel Pedraza <gabo_pedraza@yahoo.com>
2012-11-30 13:40:36 +0100

    Project Creation Wizard improved. Metadata.xml is open by default instead of MANIFEST.MF

--- code/ipojo-editor/ipojo.project.creation/src/ipojo/project/creation/wizard/IPojoPluginContentPage.java
+++ code/ipojo-editor/ipojo.project.creation/src/ipojo/project/creation/wizard/IPojoPluginContentPage.java
@@ -9,6 +9,9 @@ import org.eclipse.pde.internal.ui.wizards.plugin.NewProjectCreationPage;
 import org.eclipse.pde.internal.ui.wizards.plugin.PluginContentPage;
 
 /**
+ * 
+ * This page is created only to disable the button to create an activactor
+ * 
  * @author Gabriel
  *
  */
--- code/ipojo-editor/ipojo.project.creation/src/ipojo/project/creation/wizard/NewIPojoProjectWizard.java
+++ code/ipojo-editor/ipojo.project.creation/src/ipojo/project/creation/wizard/NewIPojoProjectWizard.java
@@ -1,45 +1,22 @@
 package ipojo.project.creation.wizard;
 
-import java.io.ByteArrayInputStream;
-import java.io.InputStream;
-import java.io.UnsupportedEncodingException;
 import java.lang.reflect.InvocationTargetException;
 
-import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IProject;
-import org.eclipse.core.resources.IProjectDescription;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IConfigurationElement;
 import org.eclipse.core.runtime.IExecutableExtension;
 import org.eclipse.core.runtime.IPath;
-import org.eclipse.core.runtime.NullProgressMonitor;
-import org.eclipse.core.runtime.Platform;
-import org.eclipse.core.runtime.preferences.IPreferencesService;
 import org.eclipse.jface.dialogs.IDialogSettings;
 import org.eclipse.jface.wizard.IWizardPage;
-import org.eclipse.pde.core.plugin.IPluginBase;
-import org.eclipse.pde.internal.core.bundle.BundlePluginBase;
-import org.eclipse.pde.internal.core.bundle.WorkspaceBundlePluginModel;
-import org.eclipse.pde.internal.core.ibundle.IBundle;
-import org.eclipse.pde.internal.core.plugin.WorkspacePluginModelBase;
-import org.eclipse.pde.internal.core.project.PDEProject;
 import org.eclipse.pde.internal.ui.PDEPlugin;
 import org.eclipse.pde.internal.ui.PDEPluginImages;
 import org.eclipse.pde.internal.ui.wizards.IProjectProvider;
 import org.eclipse.pde.internal.ui.wizards.NewWizard;
-import org.eclipse.pde.internal.ui.wizards.plugin.NewProjectCreationOperation;
 import org.eclipse.pde.internal.ui.wizards.plugin.NewProjectCreationPage;
 import org.eclipse.pde.internal.ui.wizards.plugin.PluginFieldData;
-import org.eclipse.ui.IEditorDescriptor;
-import org.eclipse.ui.IEditorInput;
-import org.eclipse.ui.IEditorReference;
-import org.eclipse.ui.IWorkbenchPage;
 import org.eclipse.ui.IWorkingSet;
-import org.eclipse.ui.PartInitException;
-import org.eclipse.ui.PlatformUI;
-import org.eclipse.ui.part.FileEditorInput;
 import org.eclipse.ui.wizards.newresource.BasicNewProjectResourceWizard;
-import org.osgi.framework.Constants;
 
 @SuppressWarnings("restriction")
 public class NewIPojoProjectWizard extends NewWizard implements IExecutableExtension {
@@ -49,7 +26,6 @@ public class NewIPojoProjectWizard extends NewWizard implements IExecutableExten
 	public static final String DEF_PROJECT_NAME = "project_name"; //$NON-NLS-1$
 	public static final String DEF_TEMPLATE_ID = "template-id"; //$NON-NLS-1$
 
-	private final static String metadataSrt = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<ipojo xmlns=\"org.apache.felix.ipojo\">\n</ipojo>";
 
 	private IConfigurationElement fConfig;
 	private PluginFieldData fPluginData;
@@ -78,16 +54,16 @@ public class NewIPojoProjectWizard extends NewWizard implements IExecutableExten
 	 * @see org.eclipse.jface.wizard.Wizard#addPages()
 	 */
 	public void addPages() {
-		
+
 		// Sets the default the project as an standard osgi project
 		IDialogSettings settings = getDialogSettings();
 		settings.put("osgiProject", true);
 		settings.put("targetName", "standard");
-		
+
 		fMainPage = new NewProjectCreationPage("main", fPluginData, false, getSelection()); //$NON-NLS-1$
 		fMainPage.setTitle("iPojo Project");
 		fMainPage.setDescription("Create a new iPojo project");
-		
+
 		String pname = getDefaultValue(DEF_PROJECT_NAME);
 		if (pname != null)
 			fMainPage.setInitialProjectName(pname);
@@ -107,12 +83,9 @@ public class NewIPojoProjectWizard extends NewWizard implements IExecutableExten
 			}
 		};
 
-
 		fContentPage = new IPojoPluginContentPage("page2", fProjectProvider, fMainPage, fPluginData); //$NON-NLS-1$
 
-
 		addPage(fContentPage);
-
 
 	}
 
@@ -142,59 +115,17 @@ public class NewIPojoProjectWizard extends NewWizard implements IExecutableExten
 			}
 			BasicNewProjectResourceWizard.updatePerspective(fConfig);
 
-
-			getContainer().run(false, true, new NewProjectCreationOperation(fPluginData, fProjectProvider, null));
+			getContainer().run(false, true, new NewPojoProjectCreationOperation(fPluginData, fProjectProvider, null));
 
 			IWorkingSet[] workingSets = fMainPage.getSelectedWorkingSets();
 			if (workingSets.length > 0)
 				getWorkbench().getWorkingSetManager().addToWorkingSets(fProjectProvider.getProject(), workingSets);
 
-			// Adds the ipojo nature to the project
-			IProject project = fMainPage.getProjectHandle();
-			IProjectDescription description = project.getDescription();
-			String[] natures = description.getNatureIds();
-			String[] newnatures = new String[natures.length + 1];
-			System.arraycopy(natures, 0, newnatures, 1, natures.length);
-			newnatures[0] = "org.ow2.chameleon.eclipse.ipojo.iPojoNature";
-			description.setNatureIds(newnatures);
-			project.setDescription(description, null);
 
-			// Creates the metadata file
-			final IFile metadataFile = project.getFile("/metadata.xml");
-			InputStream stream = new ByteArrayInputStream(metadataSrt.getBytes("UTF-8"));
-			metadataFile.create(stream, true, new NullProgressMonitor());
-
-			// Adds the imports
-
-			IPreferencesService prefService = Platform.getPreferencesService();
-			boolean addImports = prefService.getBoolean("metadata.ipojo.editor", "iCasa_Import_Preference", false, null);
-
-			if (addImports) {
-				IFile pluginXml = PDEProject.getPluginXml(project);
-				IFile manifest = PDEProject.getManifest(project);
-				WorkspacePluginModelBase fModel = new WorkspaceBundlePluginModel(manifest, pluginXml);
-				IPluginBase pluginBase = fModel.getPluginBase();
-
-				if (pluginBase instanceof BundlePluginBase) {
-					IBundle bundle = ((BundlePluginBase) pluginBase).getBundle();
-					bundle.setHeader(
-					      Constants.IMPORT_PACKAGE,
-					      "fr.liglab.adele.icasa.device, fr.liglab.adele.icasa.device.light, fr.liglab.adele.icasa.device.temperature," +
-					      "fr.liglab.adele.icasa.device.power, fr.liglab.adele.icasa.device.presence," +
-					      "fr.liglab.adele.icasa.device.sound,fr.liglab.adele.icasa.device.util");
-				}
-				fModel.save();
-			}
-			
-			closeManifest();
-			openFileInEditor(metadataFile);
-											
 			return true;
 		} catch (InvocationTargetException e) {
 			PDEPlugin.logException(e);
 		} catch (InterruptedException e) {
-		} catch (CoreException e) {
-		} catch (UnsupportedEncodingException e) {
 		}
 		return false;
 	}
@@ -220,35 +151,34 @@ public class NewIPojoProjectWizard extends NewWizard implements IExecutableExten
 		return fPluginData.getVersion();
 	}
 
-	
-	private void closeManifest() {
-		IWorkbenchPage activePage = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage();
-		IEditorReference[] references =  activePage.getEditorReferences();
-		IEditorReference manifestReference = null;
-		
-		for (IEditorReference iEditorReference : references) {
-         System.out.println(iEditorReference.getName());
-         if (iEditorReference.getName().equals("MANIFEST.MF")) {
-         	manifestReference = iEditorReference;
-         	break;
-         }
-         	
-      }
-		
-		if (manifestReference!=null) {
-			activePage.closeEditor(manifestReference.getEditor(false), false);
-		}
+	/*
+	private void closeEditorFile(final IFile file) {
+
+		PlatformUI.getWorkbench().getDisplay().asyncExec(new Runnable() {
+			public void run() {
+				IWorkbenchPage activePage = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage();
+				IEditorInput input = new FileEditorInput(file);
+				IEditorPart editor = activePage.findEditor(input);
+				if (editor != null)
+					activePage.closeEditor(editor, false);
+			}
+		});
 	}
-	
-	private void openFileInEditor(IFile file) {
-		IEditorInput editorInput = new FileEditorInput(file);
-		IEditorDescriptor desc = PlatformUI.getWorkbench().getEditorRegistry().getDefaultEditor(file.getName());
-		try {
-			PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage()
-			      .openEditor(editorInput, desc.getId());
-		} catch (PartInitException e1) {
-			e1.printStackTrace();
-		}
+
+	private void openEditorFile(final IFile file) {
+		PlatformUI.getWorkbench().getDisplay().asyncExec(new Runnable() {
+			public void run() {
+				IWorkbench workbench = PlatformUI.getWorkbench();
+				IWorkbenchPage activePage = workbench.getActiveWorkbenchWindow().getActivePage();
+				IEditorInput editorInput = new FileEditorInput(file);
+				IEditorDescriptor desc = workbench.getEditorRegistry().getDefaultEditor(file.getName());
+				try {
+					activePage.openEditor(editorInput, desc.getId());
+				} catch (PartInitException e1) {
+				}
+			}
+		});
 	}
-	
+	*/
+
 }
